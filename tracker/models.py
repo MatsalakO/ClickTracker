c56e542c4e5f306689e76a372d51df4c28f04c4a
Alex <moleksii@gmail.com>
2024-07-23 09:45:39 +0300

    Creating clicker model and changing quotes to double

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Click(models.Model):
+    ip_address = models.GenericIPAddressField()
+    browser = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.browser} - {self.timestamp}"
